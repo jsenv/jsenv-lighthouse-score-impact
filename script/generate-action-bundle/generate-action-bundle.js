2b99e8111c9ca1e0eaa7e7f1ef0e5f8d17816742
Damien Maillard <damien.maillard@dailymotion.com>
2020-06-10 15:25:26 +0200

    add action bundle

---
+++ script/generate-action-bundle/generate-action-bundle.js
@@ -0,0 +1,12 @@
+import { generateEsModuleBundle } from "@jsenv/core"
+import * as jsenvConfig from "../../jsenv.config.js"
+
+generateEsModuleBundle({
+  ...jsenvConfig,
+  bundleDirectoryClean: true,
+  bundleDirectoryRelativeUrl: "./dist/action/",
+  node: true,
+  entryPointMap: {
+    action: "./src/action.js",
+  },
+})
