f83ddfc342ac408172ad541f970ada94032a7ee8
Damien Maillard <damien.maillard@dailymotion.com>
2020-06-11 22:51:45 +0200

    dusable eslint issue, monitor google https instead

--- .github/workflows/lighthouse-impact/generate-lighthouse-report.js
+++ .github/workflows/lighthouse-impact/generate-lighthouse-report.js
@@ -1,6 +1,7 @@
+// eslint-disable-next-line import/no-unresolved
 import { generateLighthouseReport } from "@jsenv/lighthouse-score-merge-impact"
 
-export default generateLighthouseReport("http://google.com", {
+export default generateLighthouseReport("https://google.com", {
   projectDirectoryUrl: new URL("../../../", import.meta.url),
   logLevel: "debug",
 })
--- .github/workflows/lighthouse-impact/report-lighthouse-impact.js
+++ .github/workflows/lighthouse-impact/report-lighthouse-impact.js
@@ -1,6 +1,7 @@
 import {
   reportLighthouseScoreMergeImpact,
   readGithubWorkflowEnv,
+  // eslint-disable-next-line import/no-unresolved
 } from "@jsenv/lighthouse-score-merge-impact"
 
 reportLighthouseScoreMergeImpact(
