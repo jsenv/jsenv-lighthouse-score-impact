bd2888bc4cdd74abcab5839ce2d68326558845e8
Damien Maillard <damien.maillard@dailymotion.com>
2020-06-14 16:18:42 +0200

    log github ref

--- .github/workflows/test-fork.js
+++ .github/workflows/test-fork.js
@@ -2,6 +2,8 @@ import { readGithubWorkflowEnv } from "../../src/readGithubWorkflowEnv.js"
 import { getPullRequest } from "../../src/internal/pull-requests.js"
 
 const run = async () => {
+  console.log("process.env.GITHUB_REF", process.env.GITHUB_REF)
+
   const {
     repositoryOwner,
     repositoryName,
