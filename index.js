a9959f1ede5a0def1702703363dac3ced3d0516a
Damien Maillard <damien.maillard@dailymotion.com>
2020-06-14 10:59:19 +0200

    test fork

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+// testing a fork
+
 export { generateLighthouseReport } from "./src/generateLighthouseReport.js"
 export { reportLighthouseScoreMergeImpact } from "./src/reportLighthouseScoreMergeImpact.js"
 export { readGithubWorkflowEnv } from "./src/readGithubWorkflowEnv.js"
