7a697635ab0eeaeeca1bd9cf93c99c452f18e7ea
Damien Maillard <damien.maillard@dailymotion.com>
2020-08-21 14:54:08 +0200

    update some files

--- script/generate-lighthouse-report/generate-lighthouse-report.js
+++ script/generate-lighthouse-report/generate-lighthouse-report.js
@@ -3,4 +3,5 @@ import { generateLighthouseReport } from "@jsenv/lighthouse-score-impact"
 generateLighthouseReport("https://reactjs.org", {
   projectDirectoryUrl: new URL("../../", import.meta.url),
   logLevel: "debug",
+  runCount: 4,
 })
